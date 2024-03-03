981569e5ad3a29224d6a360a9e57c233615dfc92
Ronny Rook <info@ronnyrook.nl>
2024-03-03 14:01:52 +0100

    Change config

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,7 @@ export default defineConfig({
   integrations: [react(), tailwind(), sitemap()],
   output: 'server',
   adapter: vercel({
+    imageService: false,
     webAnalytics: {
       enabled: true,
     },
